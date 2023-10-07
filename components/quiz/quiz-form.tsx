99e79f27b728f8cf79ae34c1baec61e288b587b3
mmemic <memicmiralem1@gmail.com>
2023-10-07 22:54:04 +0200

    update quiz name design

--- components/quiz/quiz-form.tsx
+++ components/quiz/quiz-form.tsx
@@ -1,18 +1,10 @@
+import QuizName from './quiz-name';
 import QuizQuestions from './quiz-questions';
 
 export default function QuizForm() {
   return (
     <div className='form-control w-full max-w-sm flex flex-col gap-4'>
-      <div>
-        <label className='label'>
-          <span className='label-text'>Name</span>
-        </label>
-        <input
-          type='text'
-          placeholder='Type here'
-          className='input input-bordered input-secondary w-full max-w-sm'
-        />
-      </div>
+      <QuizName />
       <QuizQuestions />
     </div>
   );
