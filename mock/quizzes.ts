1978efdc12d7919705647874f303413ff4d016e3
mmemic <memicmiralem1@gmail.com>
2023-10-07 21:49:16 +0200

    add mock data

---
+++ mock/quizzes.ts
@@ -0,0 +1,227 @@
+import { Quiz } from '@/components/types';
+
+export const mockQuizzes: Quiz[] = [
+  {
+    id: 1,
+    name: 'Enterwell Quiz 1',
+    questions: [
+      {
+        id: 1,
+        question:
+          "Who was the English mathematician and writer widely considered as the world's first computer programmer for her work on Charles Babbage's proposed mechanical general-purpose computer, the Analytical Engine?",
+        answer: 'Ada Lovelace',
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: 'Enterwell Quiz 2',
+    questions: [
+      {
+        id: 1,
+        question: 'What is the chemical symbol for the element oxygen?',
+        answer: 'O',
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: 'Enterwell Quiz 3',
+    questions: [
+      {
+        id: 1,
+        question:
+          'In which year did Christopher Columbus first reach the Americas?',
+        answer: '1492',
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: 'Enterwell Quiz 4',
+    questions: [
+      {
+        id: 1,
+        question: 'Which planet is known as the Red Planet?',
+        answer: 'Mars',
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: 'Enterwell Quiz 5',
+    questions: [
+      {
+        id: 1,
+        question: 'What is the capital city of France?',
+        answer: 'Paris',
+      },
+    ],
+  },
+  {
+    id: 6,
+    name: 'Enterwell Quiz 6',
+    questions: [
+      {
+        id: 1,
+        question: 'Who painted the Mona Lisa?',
+        answer: 'Leonardo da Vinci',
+      },
+    ],
+  },
+  {
+    id: 7,
+    name: 'Enterwell Quiz 7',
+    questions: [
+      {
+        id: 1,
+        question: 'What is the largest mammal in the world?',
+        answer: 'Blue whale',
+      },
+    ],
+  },
+  {
+    id: 8,
+    name: 'Enterwell Quiz 8',
+    questions: [
+      {
+        id: 1,
+        question: "Which element does 'O' represent on the periodic table?",
+        answer: 'Oxygen',
+      },
+    ],
+  },
+  {
+    id: 9,
+    name: 'Enterwell Quiz 9',
+    questions: [
+      {
+        id: 1,
+        question: 'What is the tallest mountain in the world?',
+        answer: 'Mount Everest',
+      },
+    ],
+  },
+  {
+    id: 10,
+    name: 'Enterwell Quiz 10',
+    questions: [
+      {
+        id: 1,
+        question: "Who wrote 'Romeo and Juliet'?",
+        answer: 'William Shakespeare',
+      },
+    ],
+  },
+  {
+    id: 11,
+    name: 'Enterwell Quiz 11',
+    questions: [
+      {
+        id: 1,
+        question: 'What is the largest planet in our solar system?',
+        answer: 'Jupiter',
+      },
+    ],
+  },
+  {
+    id: 12,
+    name: 'Enterwell Quiz 12',
+    questions: [
+      {
+        id: 1,
+        question: 'What is the chemical symbol for gold?',
+        answer: 'Au',
+      },
+    ],
+  },
+  {
+    id: 13,
+    name: 'Enterwell Quiz 13',
+    questions: [
+      {
+        id: 1,
+        question:
+          'Which famous scientist developed the theory of general relativity?',
+        answer: 'Albert Einstein',
+      },
+    ],
+  },
+  {
+    id: 14,
+    name: 'Enterwell Quiz 14',
+    questions: [
+      {
+        id: 1,
+        question: 'What is the largest organ in the human body?',
+        answer: 'Skin',
+      },
+    ],
+  },
+  {
+    id: 15,
+    name: 'Enterwell Quiz 15',
+    questions: [
+      {
+        id: 1,
+        question: 'Who painted the Starry Night?',
+        answer: 'Vincent van Gogh',
+      },
+    ],
+  },
+  {
+    id: 16,
+    name: 'Enterwell Quiz 16',
+    questions: [
+      {
+        id: 1,
+        question: 'What is the chemical symbol for iron?',
+        answer: 'Fe',
+      },
+    ],
+  },
+  {
+    id: 17,
+    name: 'Enterwell Quiz 17',
+    questions: [
+      {
+        id: 1,
+        question: 'Which gas do plants absorb from the atmosphere?',
+        answer: 'Carbon dioxide',
+      },
+    ],
+  },
+  {
+    id: 18,
+    name: 'Enterwell Quiz 18',
+    questions: [
+      {
+        id: 1,
+        question: 'Who was the first President of the United States?',
+        answer: 'George Washington',
+      },
+    ],
+  },
+  {
+    id: 19,
+    name: 'Enterwell Quiz 19',
+    questions: [
+      {
+        id: 1,
+        question: 'What is the smallest prime number?',
+        answer: '2',
+      },
+    ],
+  },
+  {
+    id: 20,
+    name: 'Enterwell Quiz 20',
+    questions: [
+      {
+        id: 1,
+        question: "Who wrote '1984'?",
+        answer: 'George Orwell',
+      },
+    ],
+  },
+];
