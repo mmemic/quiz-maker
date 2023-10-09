b5baab6a9672c97bd1a202c3733b99d53a8ddf4f
mmemic <memicmiralem1@gmail.com>
2023-10-09 04:26:25 +0200

    add search questions endpoint

--- app/api/questions/route.ts
+++ app/api/questions/route.ts
@@ -1,7 +1,15 @@
 import { prisma } from '@/prisma/client';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
-  const data = await prisma.question.findMany();
+export async function GET(request: NextRequest) {
+  const text = request.nextUrl.searchParams.get('search') ?? undefined;
+  const limitParam = request.nextUrl.searchParams.get('limit');
+  const limit = limitParam ? Number.parseInt(limitParam) : undefined;
+
+  const data = await prisma.question.findMany({
+    where: { question: { contains: text, mode: 'insensitive' } },
+    take: limit,
+  });
+
   return NextResponse.json(data);
 }
