bb641e01899ff9f59bc8babdc342ee8c7005e4cf
mmemic <memicmiralem1@gmail.com>
2023-10-16 01:58:27 +0200

    add ai answering feature

--- components/question/add-new-question.tsx
+++ components/question/add-new-question.tsx
@@ -4,31 +4,48 @@ import { v4 as uuidv4 } from 'uuid';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { useQuizFormContext } from '@/contexts/quiz-form.context';
 import { QuestionDTO } from '@/types/question.type';
+import { useChat } from 'ai/react';
+import { useEffect } from 'react';
+import clsx from 'clsx';
 
 type Inputs = {
   question: string;
   answer: string;
 };
 
-export type AddNewQuestionProps = {
-  // handleAddQuestions: (question: CreateQuestion[]) => void;
-  // resetAction: () => void;
-};
-
-export default function AddNewQuestion({}: // handleAddQuestions,
-// resetAction,
-AddNewQuestionProps) {
+export default function AddNewQuestion() {
   const { setQuestionAction, addQuestions } = useQuizFormContext();
   const {
     register,
     handleSubmit,
     formState: { errors },
+    watch,
+    setValue,
   } = useForm<Inputs>({
     defaultValues: {
       question: '',
       answer: '',
     },
   });
+  const { messages, setInput, handleSubmit: submitQuery } = useChat();
+  const questionValue = watch().question;
+  const answerValue = watch().answer;
+
+  useEffect(() => {
+    setInput(questionValue);
+  }, [questionValue, setInput]);
+
+  useEffect(() => {
+    const lastMessage = messages[messages.length - 1]?.content;
+
+    if (lastMessage && answerValue != lastMessage) {
+      if (messages.length % 2) {
+        setValue('answer', '...');
+      } else {
+        setValue('answer', lastMessage);
+      }
+    }
+  }, [messages, answerValue, setValue]);
 
   const createValidationOptions = (name: string) => ({
     required: `${name} is required`,
@@ -38,6 +55,7 @@ AddNewQuestionProps) {
     },
     setValueAs: (val: string) => val.trim(),
   });
+
   const validationOptions = {
     question: createValidationOptions('Question'),
     answer: createValidationOptions('Answer'),
@@ -55,13 +73,12 @@ AddNewQuestionProps) {
     };
     addQuestions([newQuestion]);
     setQuestionAction(null);
-    // resetAction();
   };
 
   return (
-    <div className='flex flex-col gap-4 w-full'>
+    <div className='flex flex-col w-full'>
       <form
-        className='form-control flex flex-col p-2'
+        className='form-control flex flex-col'
         onSubmit={handleSubmit(onSubmit)}
       >
         <label className='label'>
@@ -73,7 +90,22 @@ AddNewQuestionProps) {
           {...register('question', validationOptions.question)}
         />
         <p className='text-error text-sm p-2'>{errors.question?.message}</p>
-        <label className='label'>
+      </form>
+      <form onSubmit={submitQuery} className='flex justify-end'>
+        <Button
+          className={clsx('btn btn-secondary', {
+            'btn-disabled': questionValue.length < 10,
+          })}
+          type='submit'
+        >
+          Let AI answer
+        </Button>
+      </form>
+      <form
+        className='form-control flex flex-col'
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <label className='label flex items-end'>
           <span className='label-text'>Answer</span>
         </label>
         <textarea
